967ede9a490c632660f5fe38931709794c8a65fa
Aleksandar Filipovski <aleksandar.filipovski@codechem.com>
2024-08-12 10:16:53 +0200

    add migration

--- src/gdelt_api/database/models.py
+++ src/gdelt_api/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import CHAR, Column, Date, Float
+from sqlalchemy import CHAR, Column, Date, Float, Index
 from sqlalchemy.orm import DeclarativeBase  # type: ignore
 
 
@@ -7,7 +7,14 @@ class Base(DeclarativeBase):  # type:ignore
 
 
 class RelationsModel(Base):
+    __tablename__ = "relations"
+
     date = Column(Date, primary_key=True)
     country_code_a = Column(CHAR(3), primary_key=True)
     country_code_b = Column(CHAR(3), primary_key=True)
     relations_score = Column(Float, nullable=False)
+
+    __table_args__ = (
+        Index("idx_countrya_date", "country_code_a", "date"),
+        Index("idx_countrya_countryb", "country_code_a", "country_code_b"),
+    )
