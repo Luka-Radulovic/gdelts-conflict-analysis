d1ddb681eb7e8e411f6389829769e69b09901f57
Aleksandar Filipovski <aleksandar.filipovski@codechem.com>
2024-08-12 10:54:51 +0200

    querying

--- src/gdelt_api/repository/relations_repository.py
+++ src/gdelt_api/repository/relations_repository.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from gdelt_api.database.models import RelationsModel
 from gdelt_api.database.session import session_scope
 
@@ -6,3 +8,44 @@ def add_relations(relations: RelationsModel) -> RelationsModel:
     with session_scope() as session:
         session.add(relations)
     return relations
+
+
+def get_relations_by_country_and_date(country_code: str, date: date) -> list[RelationsModel]:
+    with session_scope() as session:
+        result = (
+            session.query(RelationsModel)
+            .filter(RelationsModel.country_code_a == country_code, RelationsModel.date == date)
+            .all()
+        )
+    return result
+
+
+def get_relations_by_two_countries(
+    country_code_a: str, country_code_b: str
+) -> list[RelationsModel]:
+    with session_scope() as session:
+        result = (
+            session.query(RelationsModel)
+            .filter(
+                RelationsModel.country_code_a == country_code_a,
+                RelationsModel.country_code_b == country_code_b,
+            )
+            .all()
+        )
+    return result
+
+
+def get_relations_by_composite_id(
+    country_code_a: str, country_code_b: str, date: date
+) -> RelationsModel | None:
+    with session_scope() as session:
+        result = (
+            session.query(RelationsModel)
+            .filter(
+                RelationsModel.country_code_a == country_code_a,
+                RelationsModel.country_code_b == country_code_b,
+                RelationsModel.date == date,
+            )
+            .first()
+        )
+    return result
--- src/gdelt_api/routers/relations_router.py
+++ src/gdelt_api/routers/relations_router.py
@@ -1,9 +1,9 @@
 from datetime import date
-from fastapi import APIRouter
+
+from fastapi import APIRouter, HTTPException
 
 from gdelt_api.schema import RelationsSchema
 from gdelt_api.services import relations_service
-
 
 router: APIRouter = APIRouter(prefix="/relations")
 
@@ -13,6 +13,18 @@ async def add_relations(relations: RelationsSchema) -> RelationsSchema:
     return relations_service.add_relations(relations)
 
 
-@router.get("")
-async def get_relations_country_date(country_code: str, date: date) -> list[RelationsSchema]:
-    return relations_service.get_relations_country_date(country_code, date)
+@router.get("/")
+async def get_relations(
+    country_code_a: str = "", country_code_b: str = "", date: date | None = None
+) -> list[RelationsSchema] | RelationsSchema:
+    if country_code_a and country_code_b and date:
+        return relations_service.get_relations_by_composite_id(country_code_a, country_code_b, date)
+    elif country_code_a and country_code_b:
+        return relations_service.get_relations_by_two_countries(country_code_a, country_code_b)
+    elif country_code_a and date:
+        return relations_service.get_relations_by_country_and_date(country_code_a, date)
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="Relations can be queried by supplying either: both country codes, one country code and a date, or both country codes and a date.",
+        )
