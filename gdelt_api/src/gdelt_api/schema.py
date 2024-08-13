3ef0239522042555e74111ab23c8b40e2f66db96
Aleksandar Filipovski <aleksandar.filipovski@codechem.com>
2024-08-13 09:42:06 +0200

    add quadclass columns

--- gdelt_api/src/gdelt_api/schema.py
+++ gdelt_api/src/gdelt_api/schema.py
@@ -8,3 +8,7 @@ class RelationsSchema(BaseModel):
     country_code_a: str
     country_code_b: str
     relations_score: float
+    num_verbal_coop: int
+    num_material_coop: int
+    num_verbal_conf: int
+    num_material_conf: int
