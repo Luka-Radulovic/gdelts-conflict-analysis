22c0e20bc6b53769d8fcd746c587d4d7681d3f07
afilipovski <filipovskia02@gmail.com>
2024-09-09 13:00:10 +0200

    make schema field names match model

--- src/gdelt_api/schema.py
+++ src/gdelt_api/schema.py
@@ -4,6 +4,6 @@ from datetime import date
 
 class RelationsSchema(BaseModel):
     date: date
-    country_a: str
-    country_b: str
+    country_code_a: str
+    country_code_b: str
     relations_score: float
--- src/gdelt_api/services/relations_service.py
+++ src/gdelt_api/services/relations_service.py
@@ -1,3 +1,4 @@
+from datetime import date
 from gdelt_api.database.models import RelationsModel
 from gdelt_api.repository import relations_repository
 from gdelt_api.schema import RelationsSchema
@@ -7,3 +8,7 @@ def add_relations(relations: RelationsSchema) -> RelationsSchema:
     model = RelationsModel(**relations.model_dump())
     result = relations_repository.add_relations(model)
     return RelationsSchema.model_construct(**result.__dict__)
+
+
+def get_relations_country_date(country_code: str, date: date) -> list[RelationsSchema]:
+    pass
