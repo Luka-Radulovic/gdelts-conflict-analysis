2697d131d74dafaa5ea879a34158bc45cb612607
afilipovski <filipovskia02@gmail.com>
2024-09-09 17:57:21 +0200

    Add type hints for leaflet

--- gdelt_frontend/mapSetup.js
+++ gdelt_frontend/mapSetup.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([20, 10], 2.2);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
