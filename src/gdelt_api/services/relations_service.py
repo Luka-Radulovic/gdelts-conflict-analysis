56478f216b0db6ef557ca9635ff2e38c65cfc5df
afilipovski <filipovskia02@gmail.com>
2024-09-09 13:00:12 +0200

    handle country codes permutations

--- src/gdelt_api/services/relations_service.py
+++ src/gdelt_api/services/relations_service.py
@@ -6,6 +6,8 @@ from gdelt_api.schema import RelationsSchema
 
 
 def add_relations(relations: RelationsSchema) -> RelationsSchema:
+    if relations.country_code_a > relations.country_code_b:
+        relations.country_code_a, relations.country_code_b = relations.country_code_b, relations.country_code_a
     model = RelationsModel(**relations.model_dump())
     result = relations_repository.add_relations(model)
     return RelationsSchema.model_construct(**result.__dict__)
@@ -21,6 +23,8 @@ def get_relations_by_country_and_date(country_code: str, date: date) -> list[Rel
 def get_relations_by_two_countries(
     country_code_a: str, country_code_b: str
 ) -> list[RelationsSchema]:
+    if country_code_a > country_code_b:
+        country_code_a, country_code_b = country_code_b, country_code_a
     return [
         RelationsSchema.model_construct(**relations.__dict__)
         for relations in relations_repository.get_relations_by_two_countries(
@@ -32,6 +36,8 @@ def get_relations_by_two_countries(
 def get_relations_by_composite_id(
     country_code_a: str, country_code_b: str, date: date
 ) -> RelationsSchema:
+    if country_code_a > country_code_b:
+        country_code_a, country_code_b = country_code_b, country_code_a
     return RelationsSchema.model_construct(
         **relations_repository.get_relations_by_composite_id(
             country_code_a, country_code_b, date
