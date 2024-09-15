1258f705850dd1ee58d3e244e7d8176320d658c0
afilipovski <filipovskia02@gmail.com>
2024-09-15 17:26:37 +0200

    Allow all cors

--- gdelt_api/src/gdelt_api/main.py
+++ gdelt_api/src/gdelt_api/main.py
@@ -8,14 +8,9 @@ ROUTE_PREFIX = "/api/v1"
 app = FastAPI()
 app.include_router(relations_router.router, prefix=ROUTE_PREFIX)
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
