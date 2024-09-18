4b02ddc3ed54c31b49ffdeb3c9104c1edc2f77cb
afilipovski <filipovskia02@gmail.com>
2024-09-18 21:19:36 +0200

    njefnj

--- gdelt_frontend/src/types/RelationsSchema.ts
+++ gdelt_frontend/src/types/RelationsSchema.ts
@@ -10,20 +10,35 @@ export default interface RelationsSchema {
 }
 
 function sigmoid(z: number) {
-    return 1 / (1 + Math.exp(-(z-300)));
+    return 1 / (1 + Math.exp(-(z - 300)));
 }
 
 export function relationsToScore(relations: RelationsSchema) {
-    // return relations.relations_score * 10
+    // return relations.relations_score
 
     let total = relations.num_verbal_coop +
         relations.num_material_coop +
         relations.num_verbal_conf +
         relations.num_material_conf
 
-    let MATERIAL_MULTIPLIER = 5
-    return 10 * sigmoid(total) * (relations.num_verbal_coop + MATERIAL_MULTIPLIER * relations.num_material_coop
+    const MATERIAL_MULTIPLIER = 4
+
+    let tone = (relations.relations_score + 10) / 20
+    let significance = sigmoid(total)
+    let eventScore = ((relations.num_verbal_coop + MATERIAL_MULTIPLIER * relations.num_material_coop
         - relations.num_verbal_conf - MATERIAL_MULTIPLIER * relations.num_material_conf) /
         (relations.num_verbal_coop + MATERIAL_MULTIPLIER * relations.num_material_coop
-            + relations.num_verbal_conf + MATERIAL_MULTIPLIER * relations.num_material_conf)
+            + relations.num_verbal_conf + MATERIAL_MULTIPLIER * relations.num_material_conf) + 1) / 2
+
+    const toneMultiplier = 2
+    const significanceMultiplier = -1
+    const eventScoreMultiplier = 3
+
+    // return tone + significance + eventScore
+    // return significance
+    let baseRelations = 20 * (toneMultiplier * tone + 
+        significanceMultiplier * significance + 
+        eventScoreMultiplier * eventScore) / (toneMultiplier+significanceMultiplier+eventScoreMultiplier) - 10
+    
+    return baseRelations * Math.pow(significance, -significanceMultiplier)
 }
